b116fee1227e8b524fda1c4f76daef48a303ff60
junjie.chen <xiyueedu@gmail.com>
2022-08-03 22:55:43 +0800

    fix mt msg, amount error

---
+++ modules/mt/mt_burn.go
@@ -0,0 +1,41 @@
+package mt
+
+import (
+	"fmt"
+	. "github.com/kaifei-bianjie/msg-parser/modules"
+	"strings"
+)
+
+type (
+	DocMsgMTBurn struct {
+		Sender string `bson:"sender"`
+		Id     string `bson:"id"`
+		Amount string `bson:"amount"`
+		Denom  string `bson:"denom"`
+	}
+)
+
+func (m *DocMsgMTBurn) GetType() string {
+	return MsgTypeBurnMT
+}
+
+func (m *DocMsgMTBurn) BuildMsg(v interface{}) {
+	msg := v.(*MsgMTBurn)
+
+	m.Sender = msg.Sender
+	m.Id = strings.ToLower(msg.Id)
+	m.Amount = fmt.Sprint(msg.Amount)
+	m.Denom = strings.ToLower(msg.DenomId)
+}
+
+func (m *DocMsgMTBurn) HandleTxMsg(v SdkMsg) MsgDocInfo {
+	var addrs []string
+
+	msg := v.(*MsgMTBurn)
+	addrs = append(addrs, msg.Sender)
+	handler := func() (Msg, []string) {
+		return m, addrs
+	}
+
+	return CreateMsgDocInfo(v, handler)
+}
